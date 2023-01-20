849e2049475e4bdd1c653860d0e120bb319aa7b0
Vikas5686 <91067260+Vikas5686@users.noreply.github.com>
2023-01-20 18:48:34 -0500

    ok

---
+++ index.js
@@ -0,0 +1,31 @@
+const table = document.querySelector('.table')
+const container = document.querySelector('.container')
+const RandomArrayButton = document.querySelector('.RandomArray')
+
+function GanrateArray() {
+    let randomArray = Array.from({
+        length: 10
+    }, () => Math.floor(Math.random() * 100));
+
+    randomArray.forEach(i => {
+        var line = document.createElement('div')
+        line.className = "line"
+        line.style.height = `${i}px`
+        container.appendChild(line)
+    });
+}
+
+function RemoveLines() {
+    const line2 = document.querySelectorAll('.line')
+    line2.forEach((i) => {
+        i.remove()
+    })
+}
+
+RandomArrayButton.addEventListener('click', () => {
+    RemoveLines()
+    GanrateArray()
+})
+
+
+GanrateArray()
