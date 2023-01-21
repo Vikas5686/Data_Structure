1e11dfe6d8a1a144976b7a9cd04572149bc5ac1a
Vikas5686 <91067260+Vikas5686@users.noreply.github.com>
2023-01-20 20:39:55 -0500

    ok

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
+//Htmldocument
 const container = document.querySelector('.container')
 const RandomArrayButton = document.querySelector('.RandomArray')
+const InsertionSorting = document.querySelector('.InsertionSort')
+
+//variables
+let NUM = 10;
+let randomArray = []
+
+//funtions
+function Swaping(){
+    randomArray.forEach(i => {
+        var line = document.createElement('div')
+        line.className = "line"
+        line.style.height = `${i}px`
+        container.appendChild(line)
+    });
+}
 
 function GanrateArray() {
-    let randomArray = Array.from({
-        length: 10
-    }, () => Math.floor(Math.random() * 100));
+    randomArray = Array.from({
+        length: NUM
+    }, () => Math.floor(Math.random() * 100)+10);
 
     randomArray.forEach(i => {
         var line = document.createElement('div')
@@ -21,10 +37,32 @@ function RemoveLines() {
     })
 }
 
+function InsertionSort() {
+    for (let i = 1; i <= NUM - 1; i++) {
+        let key = randomArray[i]
+        let j = i - 1
+        while (j >= 0 && randomArray[j] > key) {
+            randomArray[j + 1] = randomArray[j]
+            j--
+        }
+        randomArray[j + 1] = key
+    }
+    RemoveLines()
+    Swaping()
+}
+
+//EventListener -click
 RandomArrayButton.addEventListener('click', () => {
     RemoveLines()
     GanrateArray()
 })
 
+InsertionSorting.addEventListener('click', () => {
+    InsertionSort();
+    console.log(randomArray)
+    
+})
 
+//Onloading
 GanrateArray()
+
