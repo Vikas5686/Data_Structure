4ebc365a8b83594204017ef648798fa4ae088efe
Vikas5686 <91067260+Vikas5686@users.noreply.github.com>
2023-04-02 21:09:40 +0530

    ok

--- index.js
+++ index.js
@@ -37,34 +37,66 @@ function RemoveLines() {
     })
 }
 
-function InsertionSort() {
-    // for (let i = 1; i <= NUM - 1; i++) {
-    //     let key = randomArray[i]
-    //     let j = i - 1
-    //     while (j >= 0 && randomArray[j] > key) {
-    //             randomArray[j + 1] = randomArray[j]
-    //         j--
-    //         RemoveLines()
-    //         Swaping()
-    //     }
-    //     randomArray[j + 1] = key
-    //     RemoveLines()
-    //     Swaping()
-    // }
-    // RemoveLines()
-    // Swaping()
-
-
-
-    for (let i = 1; i < NUM;
-        setTimeout(() => {
-            i++
-        }, 1000)
-    ) {
-
-        console.log(randomArray);
-    }
+function delay(){
+   return new Promise(resolve => { 
+        setTimeout(() => { resolve('this') }, 500); 
+    }) 
 }
+
+async function InsertionSort() {
+    // const ele = document.querySelectorAll(".line");
+    // // color
+    // console.log(ele[0])
+    // ele[0].style.background = 'green';
+    // for (let i = 1; i < ele.length; i++) {
+    //     let j = i - 1;
+    //     let key = ele[i].style.height;
+    //     // color
+    //     ele[i].style.background = 'blue';
+
+    //     // await waitforme(delay);
+
+    //     while (j >= 0 && (parseInt(ele[j].style.height) > parseInt(key))) {
+    //         // color
+    //         ele[j].style.background = 'blue';
+    //         ele[j + 1].style.height = ele[j].style.height;
+    //         j--;
+
+    //         // await waitforme(delay);
+
+    //         // color
+    //         for (let k = i; k >= 0; k--) {
+    //             ele[k].style.background = 'green';
+    //         }
+    //     }
+    //     ele[j + 1].style.height = key;
+    //     // color
+    //     ele[i].style.background = 'green';
+    // }
+
+
+    const lines = document.querySelectorAll('.line')
+    console.log(lines[0].style.height)
+    for (let i = 1; i < lines.length; i++) {
+        let key = lines[i].style.height;
+       
+        let j = i - 1;
+        await delay()
+        while (j >= 0 && (parseInt(lines[j].style.height) > parseInt(key))) {
+            lines[i].style.background="red"
+            lines[j].style.background="red"
+            lines[j + 1].style.height = lines[j].style.height
+            lines[j+1].style.background="green"
+            j--;
+            await delay()
+        }
+        lines[j + 1].style.height = key
+    }
+
+}
+
+
+
 
 //EventListener -click
 RandomArrayButton.addEventListener('click', () => {
